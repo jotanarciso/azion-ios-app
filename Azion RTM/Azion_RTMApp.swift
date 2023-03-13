64e96e7fbc1eb3662583dfa940e977542d1cf2bc
jotanarciso <joaoznarciso@gmail.com>
2023-03-13 17:58:56 -0300

    Initial Commit

---
+++ Azion RTM/Azion_RTMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Azion_RTMApp.swift
+//  Azion RTM
+//
+//  Created by Joao Narciso on 13/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Azion_RTMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
